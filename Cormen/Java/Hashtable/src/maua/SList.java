4cbf5f4a1799c00b640f80905097b916cd0b444f
Enrico <rico500000@gmail.com>
2022-08-23 17:57:16 -0300

    Hashtable Parte A e B

---
+++ Cormen/Java/Hashtable/src/maua/SList.java
@@ -0,0 +1,54 @@
+package maua;
+
+public class SList {
+    Integer value;
+    SList proximo;
+
+    public SList() {
+        this.value = 0;
+        this.proximo = null;
+    }
+
+    public SList(Integer key) {
+        this.value = key;
+        this.proximo = null;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public SList getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(SList proximo) {
+        this.proximo = proximo;
+    }
+
+    public void percorreLista() {
+        while (this.proximo != null)
+            this.proximo.percorreLista();
+    }
+
+    public void teste() {
+        System.out.println("teste");
+    }
+
+    public void InsereInicio(Integer endereco, SList[] vetor) {
+
+        if (vetor[endereco] == null)
+            vetor[endereco] = this;
+
+        else {
+            this.proximo = vetor[endereco];
+            vetor[endereco] = this;
+        }
+
+    }
+
+}
---
+++ Cormen/Java/Hashtable/src/maua/TestHash.java
@@ -0,0 +1,43 @@
+package maua;
+
+public class TestHash {
+
+    public static void main(String[] args) {
+        Integer N = 20; // N do exercicio
+        Integer[] tabKeys = new Integer[N];
+        SList[] tabHash = new SList[10];
+
+        for (Integer i = 1; i < N; i++) {
+            tabKeys[i] = i;
+        }
+        for (Integer i = 1; i < N; i++) {
+            toSList(i, tabHash);
+        }
+
+        Integer indice = 0;
+        for (SList i : tabHash) {
+            System.out.print("[" + indice + "]");
+            if (i.proximo != null) {
+                SList no = i;
+                do {
+                    System.out.print(" -> " + no.value);
+                    no = no.proximo;
+                } while (no.proximo != null);
+                System.out.println(" -> " + no.value);
+            } else {
+                System.out.println(" -> " + i.value);
+            }
+            indice++;
+        }
+    }
+
+    public static void toSList(Integer key, SList[] vetor) {
+        SList node = new SList(key);
+        node.InsereInicio(hash(key), vetor);
+    }
+
+    public static Integer hash(Integer key) {
+        return (key % 10);
+    }
+
+}
